0a7d473be4796ccf9a9cd4639f07574f5f0cc0d2
Nicholas Comuni <nick.comuni@outlook.com>
2023-02-03 11:10:14 -0300

    Aula 11

---
+++ Aula 11/my_package/meu_modulo1.py
@@ -0,0 +1,5 @@
+def my_function1():
+    print("Minha função 1 - Módulo 1")
+    
+def my_function2():
+    print("Minha função 1 - Módulo 2")
---
+++ Aula 11/my_package/meu_modulo2.py
@@ -0,0 +1,5 @@
+def my_function1():
+    print("My function1 - Módulo 2")
+    
+def my_function2():
+    print("My function2 - Módulo 2")
